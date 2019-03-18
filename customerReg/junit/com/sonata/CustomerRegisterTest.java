45d95242ff48e2437cee09d869a357501c7b9ddb
Nikhil Dev <nikhildev203@gmail.com>
2019-03-18 16:39:02 +0530

    'second'

---
+++ customerReg/junit/com/sonata/CustomerRegisterTest.java
@@ -0,0 +1,24 @@
+package com.sonata;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerRegisterTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testDoPostHttpServletRequestHttpServletResponse() {
+		 
+	}
+
+}
